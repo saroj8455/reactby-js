6497b857b0585816ffb2286d039505ad5de8268d
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-13 01:47:34 +0530

    install and configure bootstrap and bootstrap icons

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <section className='welcome'>
+      <div className='container'>
+        <h1 className='display-1 '>Hello World App</h1>
+        <button className='btn btn-primary '>
+          <i className='bi bi-amazon'></i> Click Me
+        </button>
+      </div>
+    </section>
+  );
+}
